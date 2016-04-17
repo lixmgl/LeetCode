162bd33cee187aaa530f8cabf7d1dfe58319d441
lixmgl <lixmgl@gmail.com>
2016-04-17 14:57:38 -0400

    #102:DFS solution, faster

---
+++ BinaryTreeLevelOrderDFS.java
@@ -0,0 +1,17 @@
+public class BinaryTreeLevelOrderDFS {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>>result=new ArrayList<List<Integer>>();
+        dfs(result,root,0);
+        return result;
+    }
+    
+    public void dfs(List<List<Integer>>result,TreeNode root, int level){
+        if(root==null)return;
+        if(result.size()==level){
+            result.add(new ArrayList<Integer>());
+        }
+        result.get(level).add(root.val);
+        dfs(result,root.left,level+1);
+        dfs(result,root.right,level+1);
+    }
+}
